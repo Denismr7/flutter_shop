ba457980aac55dc88eb78d2601231851a87dc190
Denis Mory <denismr7@gmail.com>
2021-06-20 22:48:10 +0200

    Added snackbars and alert dialogs

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -44,6 +44,20 @@ class ProductItem extends StatelessWidget {
           trailing: IconButton(
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'Added item to cart!',
+                  ),
+                  action: SnackBarAction(
+                    label: 'UNDO',
+                    onPressed: () {
+                      cart.removeSingleItem(product.id);
+                    },
+                  ),
+                ),
+              );
             },
             color: Theme.of(context).accentColor,
             icon: Icon(Icons.shopping_cart),
